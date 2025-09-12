5060a8a81603f0faa33f3753d0952e501044a67b
ShikharISM <shikharyadav885@gmail.com>
2025-09-12 23:46:04 +0530

    final commit

--- server/server.js
+++ server/server.js
@@ -118,7 +118,12 @@ app.use('/api/messages', messageRouter);
 // --- SERVER STARTUP ---
 await connectToDB();
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-    console.log(`Server is running on PORT: ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, () => {
+        console.log(`Server is running on PORT: ${PORT}`);
+    });
+}
+
+//export server for vercel
+export default server
